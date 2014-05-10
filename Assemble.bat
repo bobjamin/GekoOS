8f530c692a29725cb3d228c6ddf71bf2bb0907bf
bobjamin <xbobjamin@gmail.com>
2014-05-10 12:47:07 +0100

    Added Batch build script
    Script Assembles and includes asm files and copies over binary to
    virtual machine

---
+++ Assemble.bat
@@ -0,0 +1,51 @@
+::Remove unwanted text
+@ECHO OFF
+
+::Set up properties to reference later
+SET BootSector=.\BootSector
+SET BIN=%BootSector%\Bin
+SET ASM=%BootSector%\Asm
+SET ASMINC=%ASM%\Include\
+SET VM=.\VM
+
+::Assemble BootSector and add Include directory
+ECHO Assembling BootSector...
+ECHO.
+nasm -f bin -o %BIN%\BootSector.bin -i %ASMINC% %ASM%\BootSector.asm
+::Catch errors and exit
+IF ERRORLEVEL 1 GOTO AsmFail
+::If No errors continue
+
+::Copy Binary to the Virtual Machine directory
+:AsmComplete
+ECHO Assembly complete
+ECHO.
+ECHO Copying Binary...
+ECHO.
+copy /Y /b %BIN%\BootSector.bin %VM%\BootSector.bin
+::Catch errors and exit
+IF ERRORLEVEL 1 GOTO CopyFail
+
+::Run Bochs
+ECHO.
+ECHO Running BOCHS...
+ECHO.
+START bochsrc.lnk
+GOTO END
+
+::Echo fail message
+:AsmFail
+ECHO.
+ECHO Assembly failed
+ECHO.
+GOTO END  
+
+::Echo fail message
+:CopyFail
+ECHO.
+ECHO Copy failed
+ECHO.
+GOTO END
+
+:END
+PAUSE
