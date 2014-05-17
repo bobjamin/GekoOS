7066999dae01d05fe021198164e96681ecaa21d6
bobjamin <xbobjamin@gmail.com>
2014-05-17 14:01:18 +0100

    Store Variables

--- BootSector/Asm/BootSector.asm
+++ BootSector/Asm/BootSector.asm
@@ -8,6 +8,9 @@ Start:
 %include "Print16.i"  ;Include our print file
 
 WELCOMEMSG	DB	"--- Welcome to Geko (16-bit real mode) ---", 0
+DISKERRMSG	DB	"Disk Read Error!",0
+DISKSCCMSG 	DB	"Disk Read Success!", 0
+BOOTDRIVE	DB	0
 
 TIMES 510-($-$$) DB 0 
 DW 0XAA55
