26b75b6f3c3b5078d44086251caa7f974beb7164
bobjamin <xbobjamin@gmail.com>
2014-05-17 14:03:50 +0100

    Add ReadSectors Method

--- BootSector/Asm/BootSector.asm
+++ BootSector/Asm/BootSector.asm
@@ -5,6 +5,26 @@ Start:
     CLI                 ;Clear interrupts
     HLT                 ;Halt Processor
 
+;Read DH sectors to ES:BX from Drive DL
+ReadSectors:
+	PUSH DX
+	MOV AH, 0X2			;Read Disk Function
+	MOV CH, 0 			;Cylinder 0
+	MOV CL, 0X2			;2nd Sector (512 bytes)
+	MOV AL, DH			;No. Sectors
+	MOV DH, 0 			;Head 0
+	INT 0X13			;Read
+	JC .DiskError		;Error if Carry Flag set
+	POP DX
+	CMP AL, DH			;Compare No. actually copied
+	JNE .DiskError
+RET
+.DiskError:
+	MOV SI, DISKERRMSG
+	CALL  PrintString16
+	CLI
+	HLT
+
 %include "Print16.i"  ;Include our print file
 
 WELCOMEMSG	DB	"--- Welcome to Geko (16-bit real mode) ---", 0
