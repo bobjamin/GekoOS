a213eebb54ea46354caa8643811eb51f92b44326
bobjamin <xbobjamin@gmail.com>
2014-05-18 12:54:37 +0100

    Added Screen16.i
    Adds some functionality for clearing screen and setting the cursor
    position (16-bit interrupts)

--- BootSector/Asm/BootLoader.asm
+++ BootSector/Asm/BootLoader.asm
@@ -2,11 +2,16 @@
 [BITS 16]
 
 Start:
-		MOV SI, MSG
+		MOV CH, 0X0F		;Set Color to White on Black
+		MOV BH, 0X0 		;Page 0
+		CALL ClearScreen	;Clear Screen
+		MOV SI, MSG			;Print HELLO
 		CALL PrintString16
+		
 		CLI
 		HLT
 
 %include "Print16.i"
+%include "Screen16.i"
 
 MSG		DB	"HELLO",0
