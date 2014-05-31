67dff7635618a45f5723b0c790b4c5a58de4365a
bobjamin <xbobjamin@gmail.com>
2014-05-31 11:13:45 +0100

    End of Chapter 4

--- WebSite/javascripts/util.js
+++ WebSite/javascripts/util.js
@@ -24,13 +24,13 @@ var precodes = document.getElementsByClassName('nasmcode');
   {
     var codetag = precodes[j];
     var text = codetag.innerText;
-    text = text.replace(/\b(AND|CALL|CLI|CMP|HLT|INT|JC|JE|JMP|JNE|JZ|MOV|POP|PUSH|OR|RET|XOR)\b/ig,"<SPAN class='mnemonic'>$&</SPAN>");
+    text = text.replace(/\b(ADD|AND|CALL|CLI|CMP|HLT|INT|JC|JE|JG|JGE|JL|JLE|JMP|JNE|JZ|MOV|OR|POP|PUSH|RET|SHL|SHR|XOR)\b/ig,"<SPAN class='mnemonic'>$&</SPAN>");
     text = text.replace(/(\bE?([SB][IP]\b))|(\bE?[CDE]S\b)/ig,"<SPAN class='mnemonic'>$&</SPAN>");
     text = text.replace(/(\bBITS|\bORG|%include)\b/ig,"<SPAN class='preprocessor'>$&</SPAN>");
     
     text = text.replace(/\bE?[ABCD][XHL]\b/ig,"<SPAN class='register'>$&</SPAN>");
     text = text.replace(/(\b|\.)[A-Z]\w*\:/ig,"<SPAN class='label'>$&</SPAN>");
-    text = text.replace(/\bBYTE|WORD|DWORD|QWORD|(D[DBW])\b/ig,"<SPAN class='sizeallocation'>$&</SPAN>");
+    text = text.replace(/\b(BYTE|WORD|DWORD|QWORD|(D[DBW]))\b/ig,"<SPAN class='sizeallocation'>$&</SPAN>");
     text = text.replace(/(\b0X[0-9A-F]+\b)|(\b\d+\b)|(\b[0-9A-F]+H\b)|(\b[01]+b\b)/ig,"<SPAN class='number'>$&</SPAN>");
     text = text.replace(/\".*\"/ig,"<SPAN class='string'>$&</SPAN>");
     text = text.replace(/;.*/ig,"<SPAN class='comment'>$&</SPAN>");
